3256401cb667c56fb1f2d9c06423b5e7bf619b03
unknown <Mike@Mike-PC.(none)>
2011-10-31 10:51:23 -0400

    fixed array routine names

--- array.c
+++ array.c
@@ -10,35 +10,35 @@ static const int array_grow_mul = 3;
 static const int array_grow_div = 2;
 
 /* grow one increment w/ exponential scaling */
-static void array_grow(kl_array_t *array);
+static void array_grow(array_t *array);
 /* grow to at least 'size' w/ exponential scaling */
-static void array_growto(kl_array_t *array, int minsize, uint8_t clearbyte);
+static void array_growto(array_t *array, int minsize, uint8_t clearbyte);
 /* grow to exactly 'size' entries */
-static void array_resize(kl_array_t *array, int size);
+static void array_resize(array_t *array, int size);
 
 /* ------------------------ */
-void kl_array_init(kl_array_t *array, int item_size) {
+void array_init(array_t *array, int item_size) {
   array->data      = malloc(array_initial_size * item_size);
   array->size      = array_initial_size;
   array->item_size = item_size;
   array->num_items = 0;
 }
 
-void kl_array_clear(kl_array_t *array) {
+void array_clear(array_t *array) {
   free(array->data);
   array->data      = malloc(array_initial_size * array->item_size);
   array->size      = array_initial_size;
   array->num_items = 0;
 }
 
-void kl_array_free(kl_array_t *array) {
+void array_free(array_t *array) {
   free(array->data);
   array->size      = 0;
   array->item_size = 0;
   array->num_items = 0;
 }
 
-int kl_array_push(kl_array_t *array, void *item) {
+int array_push(array_t *array, void *item) {
   int i = array->num_items++;
   if (array->num_items > array->size) {
     array_grow(array);
@@ -49,22 +49,22 @@ int kl_array_push(kl_array_t *array, void *item) {
   return i;
 }
 
-void kl_array_set_expand(kl_array_t *array, int i, void *item, uint8_t clearbyte) {
+void array_set_expand(array_t *array, int i, void *item, uint8_t clearbyte) {
   if (i < array->num_items) {
-    kl_array_set(array, i, item);
+    array_set(array, i, item);
     return;
   }
   array_growto(array, i+1, clearbyte);
   array->num_items = i + 1;
-  kl_array_set(array, i, item);
+  array_set(array, i, item);
 }
 
-static void array_grow(kl_array_t *array) {
+static void array_grow(array_t *array) {
   int size = array->size * array_grow_mul / array_grow_div;
   array_resize(array, size);
 }
 
-static void array_growto(kl_array_t *array, int minsize, uint8_t clearbyte) {
+static void array_growto(array_t *array, int minsize, uint8_t clearbyte) {
   int oldsize = array->num_items;
   int size    = array->size;
   while (size < minsize) {
@@ -74,7 +74,7 @@ static void array_growto(kl_array_t *array, int minsize, uint8_t clearbyte) {
   memset(array->data + oldsize * array->item_size, clearbyte, (minsize - oldsize) * array->item_size);
 }
 
-static void array_resize(kl_array_t *array, int size) {
+static void array_resize(array_t *array, int size) {
   array->data = realloc(array->data, size * array->item_size);
   array->size = size;
 }
