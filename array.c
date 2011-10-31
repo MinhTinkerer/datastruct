72a15a629806fad3fb5e0b5103d902290739a1b4
unknown <Mike@Mike-PC.(none)>
2011-10-31 08:43:46 -0400

    copying over dynamic array rewrite from kludge-engine

--- array.c
+++ array.c
@@ -3,31 +3,79 @@
 #include <stdlib.h>
 #include <assert.h>
 
-/* start with 0x1000 elements (not bytes) */
-static const int array_initial_size = 0x1000;
-/* new size / old size = 3 / 2 */
-static const int array_grow_mul     = 3;
-static const int array_grow_div     = 2;
+/* start with 0x100 elements (not bytes) */
+static const int array_initial_size = 0x100;
+/* new size / old size = 3/2 */
+static const int array_grow_mul = 3;
+static const int array_grow_div = 2;
 
-static void array_grow(array_t *array) {
-  array->size  = array->size * array_grow_mul / array_grow_div;
-  array->items = realloc(array->items, array->size * sizeof(void*));
+/* grow one increment w/ exponential scaling */
+static void array_grow(kl_array_t *array);
+/* grow to at least 'size' w/ exponential scaling */
+static void array_growto(kl_array_t *array, int minsize, uint8_t clearbyte);
+/* grow to exactly 'size' entries */
+static void array_resize(kl_array_t *array, int size);
+
+/* ------------------------ */
+void kl_array_init(kl_array_t *array, int item_size) {
+  array->data      = malloc(array_initial_size * item_size);
+  array->size      = array_initial_size;
+  array->item_size = item_size;
+  array->num_items = 0;
 }
 
-void  array_init(array_t *array) {
-  array->items   = malloc(array_initial_size * sizeof(void*));
-  array->size    = array_initial_size;
-  array->n       = 0;
+void kl_array_clear(kl_array_t *array) {
+  free(array->data);
+  array->data      = malloc(array_initial_size * array->item_size);
+  array->size      = array_initial_size;
+  array->num_items = 0;
 }
 
-int   array_push(array_t *array, void *item) {
-  int i = array->n++;
-  if (array->n > array->size) {
+void kl_array_free(kl_array_t *array) {
+  free(array->data);
+  array->size      = 0;
+  array->item_size = 0;
+  array->num_items = 0;
+}
+
+int kl_array_push(kl_array_t *array, void *item) {
+  int i = array->num_items++;
+  if (array->num_items > array->size) {
     array_grow(array);
     assert(i < array->size);
   }
-  array->items[i] = item;
+  int bytes = array->item_size;
+  memcpy(array->data + i * bytes, item, bytes);
   return i;
 }
 
-/* vim: set ts=2 sts=2 sw=2 et ft=c tw=120: */
+void kl_array_set_expand(kl_array_t *array, int i, void *item, uint8_t clearbyte) {
+  if (i < array->num_items) {
+    kl_array_set(array, i, item);
+    return;
+  }
+  array_growto(array, i+1, clearbyte);
+  array->num_items = i + 1;
+  kl_array_set(array, i, item);
+}
+
+static void array_grow(kl_array_t *array) {
+  int size = array->size * array_grow_mul / array_grow_div;
+  array_resize(array, size);
+}
+
+static void array_growto(kl_array_t *array, int minsize, uint8_t clearbyte) {
+  int oldsize = array->num_items;
+  int size    = array->size;
+  while (size < minsize) {
+    size = size * array_grow_mul / array_grow_div;
+  }
+  array_resize(array, size);
+  memset(array->data + oldsize * array->item_size, clearbyte, (minsize - oldsize) * array->item_size);
+}
+
+static void array_resize(kl_array_t *array, int size) {
+  array->data = realloc(array->data, size * array->item_size);
+  array->size = size;
+}
+/* vim: set ts=2 sw=2 et */
--- array.h
+++ array.h
@@ -2,42 +2,54 @@
 #define ARRAY_H
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include <assert.h>
 
 typedef struct array {
-  void      **items;
-  int         size;
-  int         n;
+  uint8_t *data;
+  int      size;
+  int      item_size;
+  int      num_items;
 } array_t;
 
-void  array_init(array_t *array);
+void  array_init(array_t *array, int size);
+void  array_clear(array_t *array);
+void  array_free(array_t *array);
 int   array_push(array_t *array, void *item);
 
-static inline void* array_pop(array_t *array) {
-  if (array->n <= 0) { return NULL; }
-  return array->items[--array->n];
+static inline int array_pop(array_t *array, void* item) {
+  if (array->num_items <= 0) return -1;
+  int bytes = array->item_size;
+  memcpy(item, array->data + (--array->num_items) * bytes, bytes);
+  return 0;
 }
 
-static inline void* array_get(array_t *array, int i) {
-  return i < array->n ? array->items[i] : NULL;
+static inline void array_get(array_t *array, int i, void* item) {
+  assert(i < array->num_items);
+  int bytes = array->item_size;
+  memcpy(item, array->data + i * bytes, bytes);
 }
 
 static inline void  array_set(array_t *array, int i, void *item) {
-  assert(i < array->n);
-  array->items[i] = item;
+  assert(i < array->num_items);
+  int bytes = array->item_size;
+  memcpy(array->data + i * bytes, item, bytes);
+}
+void array_set_expand(array_t *array, int i, void *item, uint8_t clearbyte);
+
+static inline void* array_data(array_t *array) {
+  return array->data;
 }
 
-static inline void  array_swap(array_t *array, int i, int j) {
-  assert(i < array->n && j < array->n);
-  void* temp      = array->items[i];
-  array->items[i] = array->items[j];
-  array->items[j] = temp;
+static inline int array_bytes(array_t *array) {
+  return array->num_items * array->item_size;
 }
 
-static inline int   array_size(array_t *array) {
-  return array->n;
+static inline int array_size(array_t *array) {
+  return array->num_items;
 }
 
 #endif /* ARRAY_H */
 
-/* vim: set ts=2 sts=2 sw=2 et ft=c tw=120: */
+/* vim: set ts=2 sw=2 et */
